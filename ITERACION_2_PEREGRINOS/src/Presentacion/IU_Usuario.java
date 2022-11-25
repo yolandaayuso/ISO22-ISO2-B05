adc84b594da747680cf35962e67b6d8862933871
laurafdlm <92792838+laurafdlm@users.noreply.github.com>
2022-11-25 17:29:09 +0100

    Add files via upload

---
+++ ITERACION_2_PEREGRINOS/src/Presentacion/IU_Usuario.java
@@ -0,0 +1,10 @@
+package Presentacion;
+
+public class IU_Usuario {
+
+	public boolean clickIniciarSesion() {
+		// TODO - implement IU_Usuario.clickIniciarSesion
+		throw new UnsupportedOperationException();
+	}
+
+}
