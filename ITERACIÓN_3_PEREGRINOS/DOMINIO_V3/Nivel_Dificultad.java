96e7f67c196d49ea7fc4049e53870742403dbff0
JavierG2001 <Javier.Gonzalez42@alu.uclm.es>
2022-11-29 21:52:31 +0100

    iteración 3 peregrinos

---
+++ ITERACIÓN_3_PEREGRINOS/DOMINIO_V3/Nivel_Dificultad.java
@@ -0,0 +1,7 @@
+package ITERACIÓN_3_PEREGRINOS.DOMINIO_V3;
+
+public class Nivel_Dificultad {
+	Bajo,
+	Medio,
+	Alto
+}
---
+++ ITERACIÓN_3_PEREGRINOS/PRESENTACION_V3/IU_Peregrino.java
@@ -0,0 +1,15 @@
+package ITERACIÓN_3_PEREGRINOS.PRESENTACION_V3;
+
+public class IU_Peregrino {
+
+	public void clickHacerPublicacion() {
+		// TODO - implement IU_Peregrino.clickHacerPublicacion
+		throw new UnsupportedOperationException();
+	}
+
+	public void clickConsultarDatos() {
+		// TODO - implement IU_Peregrino.clickConsultarDatos
+		throw new UnsupportedOperationException();
+	}
+
+}
