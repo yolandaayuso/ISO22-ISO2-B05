f197e7a2e5eed7d364551ade7117abc4fffeeef9
laurafdlm <92792838+laurafdlm@users.noreply.github.com>
2022-12-08 16:09:15 +0100

    Add files via upload

---
+++ PILGRIMFY/it4_peregrinos/src/main/resources/archetype-resources/src/main/java/Presentacion/IU_Peregrino.java
@@ -0,0 +1,20 @@
+package Presentacion;
+
+public class IU_Peregrino {
+
+	public void clickHacerPublicacion() {
+		// TODO - implement IU_Peregrino.clickHacerPublicacion
+		throw new UnsupportedOperationException();
+	}
+
+	public void clickConsultarDatos() {
+		// TODO - implement IU_Peregrino.clickConsultarDatos
+		throw new UnsupportedOperationException();
+	}
+
+	public void clickGenerarSolicitud() {
+		// TODO - implement IU_Peregrino.clickGenerarSolicitud
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PILGRIMFY/it4_servidor/target/classes/archetype-resources/src/main/java/Dominio/Peregrino.java
@@ -0,0 +1,52 @@
+package Dominio;
+
+
+public class Peregrino extends Usuario {
+
+	/**
+	 * 
+	 * @param reseña
+	 */
+	public Reseña publicarComentario(String reseña) {
+		// TODO - implement Peregrino.publicarComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reseñaDenuncia
+	 * @param motivo
+	 */
+	public void denunciarComentario(Reseña reseñaDenuncia, String motivo) {
+		// TODO - implement Peregrino.denunciarComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reseñaLike
+	 */
+	public void darLikeComentario(Reseña reseñaLike) {
+		// TODO - implement Peregrino.darLikeComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param puntoAñadir
+	 */
+	public void proponerPuntoInteres(Punto_Interes puntoAñadir) {
+		// TODO - implement Peregrino.proponerPuntoInteres
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombreRuta
+	 */
+	public Ruta accederDatosRuta(String nombreRuta) {
+		// TODO - implement Peregrino.accederDatosRuta
+		throw new UnsupportedOperationException();
+	}
+
+}
