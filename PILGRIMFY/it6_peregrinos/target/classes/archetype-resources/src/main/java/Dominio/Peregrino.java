b200a79e223e2789079273f3d7c79f2702f5d096
laurafdlm <92792838+laurafdlm@users.noreply.github.com>
2022-12-08 16:07:41 +0100

    Add files via upload

---
+++ PILGRIMFY/it6_peregrinos/target/classes/archetype-resources/src/main/java/Dominio/Peregrino.java
@@ -0,0 +1,75 @@
+package Dominio;
+
+public class Peregrino extends Usuario {
+
+	/**
+	 * 
+	 * @param reseña
+	 */
+	public Reseña publicarComentario(String reseña) {
+		// TODO - implement Peregrino.publicarComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reseñaLike
+	 */
+	public void darLikeComentario(Reseña reseñaLike) {
+		// TODO - implement Peregrino.darLikeComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param reseñaDenuncia
+	 * @param motivo
+	 */
+	public void denunciarComentario(Reseña reseñaDenuncia, String motivo) {
+		// TODO - implement Peregrino.denunciarComentario
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombreRuta
+	 */
+	public Ruta accederDatosRuta(String nombreRuta) {
+		// TODO - implement Peregrino.accederDatosRuta
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param puntoAñadir
+	 */
+	public void proponerPuntoInteres(Punto_Interes puntoAñadir) {
+		// TODO - implement Peregrino.proponerPuntoInteres
+		throw new UnsupportedOperationException();
+	}
+
+	public Oferta[] listarOfertasRecibidas() {
+		// TODO - implement Peregrino.listarOfertasRecibidas
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param nombreOferta
+	 */
+	public Oferta accederDatosOferta(String nombreOferta) {
+		// TODO - implement Peregrino.accederDatosOferta
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param ofertaElegida
+	 * @param horaFecha
+	 */
+	public void generarReserva(Oferta ofertaElegida, String horaFecha) {
+		// TODO - implement Peregrino.generarReserva
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ PILGRIMFY/it6_peregrinos/target/classes/archetype-resources/src/main/java/Presentacion/IU_ManejoApp.java
@@ -0,0 +1,40 @@
+package Presentacion;
+
+public class IU_ManejoApp {
+
+	public void submmitVerDatosRuta() {
+		// TODO - implement IU_ManejoApp.submmitVerDatosRuta
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitVerDatosOferta() {
+		// TODO - implement IU_ManejoApp.submmitVerDatosOferta
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitVerListaOfertas() {
+		// TODO - implement IU_ManejoApp.submmitVerListaOfertas
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitPublicarComentario() {
+		// TODO - implement IU_ManejoApp.submmitPublicarComentario
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitValorarComentarios() {
+		// TODO - implement IU_ManejoApp.submmitValorarComentarios
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitSolicitarPuntoInteres() {
+		// TODO - implement IU_ManejoApp.submmitSolicitarPuntoInteres
+		throw new UnsupportedOperationException();
+	}
+
+	public void submmitSolicitarReservaOferta() {
+		// TODO - implement IU_ManejoApp.submmitSolicitarReservaOferta
+		throw new UnsupportedOperationException();
+	}
+
+}
